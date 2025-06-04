ebf7817756f9e4f333e7abad4f8036ccb1add0fb
Furkan Recber <furkan.recber57@gmail.com>
2025-06-04 21:26:40 +0300

    test2

--- eTicaretAPI/Presentation/eTicaretAPI.API/Program.cs
+++ eTicaretAPI/Presentation/eTicaretAPI.API/Program.cs
@@ -4,6 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddPersistenceServices();      // IoC Container'a ne eklenirse çalýþacak çünkü bu komutla çaðrýlýyor
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.WithOrigins("https://localhost:4200", "http://localhost:4200")       //  Allow specific origins
+                          .AllowAnyMethod()             // Allow any HTTP method (GET, POST, PUT, DELETE, etc.)
+                          .AllowAnyHeader());           // Allow any header in the request
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(); // CORS politikasýný uygulamak için
 
 app.UseHttpsRedirection();
 
