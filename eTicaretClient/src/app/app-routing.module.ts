f1953992678fb61b821b276db9955259cfd9f574
Furkan Reçber <furkan@Furkan-MacBook-Pro.local>
2025-06-06 07:00:48 +0300

    routerlar eklendi

--- eTicaretClient/src/app/app-routing.module.ts
+++ eTicaretClient/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { HomeComponent } from './ui/components/home/home.component';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "admin", component: LayoutComponent, children: [
+    {path: "", component: DashboardComponent},  //Burada path'in boş olmasının nedeni direkt admin sayfasına (https://bilmemne/admin) gidilmek istendiğinde dashboard'ın açılması istendiği için. 
+    {path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then (module => module.CustomerModule)}, //https://bilmemne/admin/customers
+    {path: "orders", loadChildren: () => import("./admin/components/order/order.module").then (module => module.OrderModule)},
+    {path: "products", loadChildren: () => import("./admin/components/products/products.module").then (module => module.ProductsModule)}
+  ]},
+  {path: "", component: HomeComponent}, // Direkt https://bilmemne sitesine gidildiğinde açılacak sayfa
+  {path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then (module => module.BasketsModule)},
+  {path: "products", loadChildren: () => import("./ui/components/products/products.module").then (module => module.ProductsModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
